ec4622e4e7d3f4aeb884cc1a4bbb198efa3159bb
Patrick <patrick.sch@web.de>
2026-05-18 21:47:01 +0200

    responsive home updated

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,40 +3,53 @@ import { SurveyService } from '../../shared/services/survey.service';
 import { SurveyListComponent } from './survey-list/survey-list';
 import { EndingSoonComponent } from './ending-soon/ending-soon';
 import { Router } from '@angular/router';
+import { CategoryService } from '../../shared/services/category';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
-  imports: [SurveyListComponent, EndingSoonComponent],
+  imports: [
+    SurveyListComponent,
+    EndingSoonComponent,
+    FormsModule
+  ],
 })
 export class HomeComponent {
   private surveyService = inject(SurveyService);
+  private categoryService = inject(CategoryService);
 
   filter = 'active';
   endingSoon = signal<any[]>([]);
   active = signal<any[]>([]);
   past = signal<any[]>([]);
+  categoryOpen = false;
+  filterCategory = '';
+
+  categories = this.categoryService.categories;
 
   async ngOnInit() {
     this.endingSoon.set(await this.surveyService.getEndingSoon());
     this.active.set(await this.surveyService.getActiveSurveys());
     this.past.set(await this.surveyService.getPastSurveys());
   }
-  
-
 
   private router = inject(Router);
 
-  /** Navigates to the create survey page */
   goToCreate(): void {
     this.router.navigate(['/create']);
   }
 
-  /** Sets the current filter for the survey list */
   setFilter(value: 'active' | 'past') {
     this.filter = value;
   }
 
+  getFilteredSurveys() {
+    const list = this.filter === 'active' ? this.active() : this.past();
+    if (!this.filterCategory) return list;
+    return list.filter(s => s.category === this.filterCategory);
+  }
+  
 }
